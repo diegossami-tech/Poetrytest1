8972d9ad6a37c7ab21a5d1b9363ce6e80d61f148
diegossami-tech <diegossami@gmail.com>
2025-10-06 11:56:14 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Site carregado!");
+  // Adicione interatividade, como alertas ou manipulação do DOM
+  alert("Bem-vindo ao meu site!");
+});
